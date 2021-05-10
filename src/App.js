9c4446eee27c8621335510f3eb935197f560e1e2
paul <vvellper@gmail.com>
2021-05-10 11:48:47 +0300

    NavBar update

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div>
       <Router>
-        <Navbar></Navbar>
+        {/* <Navbar></Navbar> */}
         <Switch>
           <Route exact path="/">
             <Home></Home>
